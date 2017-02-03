73797c6e3558426fbaf3b0b9acc848b6e02e009f
John P. Willis <jpw@coherent-logic.com>
2017-02-03 16:43:34 -0700

    Convert to Express app

---
+++ src/web/app.js
@@ -0,0 +1,57 @@
+/*
+ * Geodigraph GIS 2017
+ *
+ * Copyright (C) 2017 Coherent Logic Development LLC
+ * 
+ * Author: John P. Willis <jpw@coherent-logic.com>
+ *
+ * app.js: main express server
+ *
+ */
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static('public'));
+
+app.use('/', routes);
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/geodigraph-auth');
+
+var config = require('./gis-config.js');
+
+app.listen(config.listener.port, function () {
+    console.log("Geodigraph GIS 2017");
+    console.log(" Copyright (C) 2017 Coherent Logic Development LLC\n\n");
+    console.log("Listening at port " + config.listener.port);
+});
