396d86a7ed84bef472466eb7d163b796a38a933f
John P. Willis <jpw@coherent-logic.com>
2017-11-03 14:16:05 -0600

    Foo

--- src/web/public/js/app.js
+++ src/web/public/js/app.js
@@ -1,14 +1,31 @@
 var map = null;
 var app = {    
     init: function() {	
-	$.get("/config.json", function(data) {
-		var vectorLayers = [
-			'http://dev.geodigraph.com:3000/vectorLayers'
-		];
-		var rasterLayers = [
-			'http://dev.geodigraph.com:3000/rasterLayers'
-		];
-	    map = new geodigraph.gis.Map(data, vectorLayers, rasterLayers);	    
+	$.get("/config.json", function(data) {			
+	    map = new Map("root", data.defaults);	
 	});
     }
 };
+
+/*
+
+    for(serviceIndex in rasterLayers) {
+        $.get(rasterLayers[serviceIndex], function(layers) {
+            for(layerIndex in layers) {
+    	   L.tileLayer(layers[layerIndex].url, {
+    	       attribution: layers[layerIndex].attribution,
+    	       maxZoom: self.opts.defaults.maxZoom
+    	       }).addTo(self.leafletMap);
+    	   }    	
+        });
+    }
+
+    for(serviceIndex in vectorLayers) {
+        $.get(vectorLayers[serviceIndex], function(layers) {
+            for(layerIndex in layers) {
+                console.log(layers[layerIndex]);
+            }
+        });
+    }    
+    
+    */
--- src/web/public/js/gis.js
+++ src/web/public/js/gis.js
@@ -9,77 +9,94 @@
  *
  */
 
-function __giMap(tagOpts, vectorLayers, rasterLayers)
+function Map(elementId, opts)
 {
-    this.opts = tagOpts || {};
-    self = this;
+    this.opts = opts || {};
+    var self = this;
 
-    this.plugins = [];
-    this.rasterLayers = [];
     this.vectorLayers = [];
+    this.rasterLayers = [];
+    this.basemapLayers = [];
 
-    this.leafletMap = L.map('root', {
-		center: new L.LatLng(self.opts.defaults.lat, self.opts.defaults.lon),
-		zoom: self.opts.defaults.zoom,
-		zoomControl: true,
-		zoomsliderControl: true,
-		minZoom: self.opts.defaults.minZoom,
-		maxZoom: self.opts.defaults.maxZoom
+    this.leafletMap = L.map(elementId, {
+        center: new L.LatLng(opts.lat, opts.lon),
+        zoom: opts.zoom,
+        minZoom: opts.minZoom,
+        maxZoom: opts.maxZoom
     });
+
     
-    for(serviceIndex in rasterLayers) {
-    	$.get(rasterLayers[serviceIndex], function(layers) {
-    		for(layerIndex in layers) {
-    			L.tileLayer(layers[layerIndex].url, {
-    				attribution: layers[layerIndex].attribution,
-    				maxZoom: self.opts.defaults.maxZoom
-    			}).addTo(self.leafletMap);
-    		}    	
-    	});
+    return this;
+}
+
+Map.prototype.installLayer = function (layer) {
+   
+    if(layer instanceof BasemapLayer) {
+        this.basemapLayers.push(layer)
     }
-    
-    
-    
-    return self;
-}
-
-function __giLayer(tagOpts)
-{
-    this.opts = tagOpts || {};
-    self = this;
-
-    return self;
-}
-
-function __giViewport(tagOpts)
-{
-    this.opts = tagOpts || {};
-    self = this;
-
-    return self;
-}
-
-function __giStatus(tagOpts)
-{
-    this.opts = tagOpts || {};
-    self = this;
-
-    return self;
-}
-
-function __giPlugin(tagOpts)
-{
-    this.opts = tagOpts || {};
-    self = this;
-
-    return self;
-}
-
-var geodigraph = {
-    gis: {
-    	Map: __giMap,
-    	Layer: __giLayer,
-    	Viewport: __giViewport,
-    	Plugin: __giPlugin
+    else if(layer instanceof RasterLayer) {
+        this.rasterLayers.push(layer);
     }
+    else if(layer instanceof VectorLayer) {
+        this.vectorLayers.push(layer);
+    }
+    else {
+        console.log("Map.installLayer(): invalid layer type; cannot install (layers must be of type BasemapLayer, VectorLayer, or RasterLayer)");
+
+        return;
+    }
+
+    layer.install(this);
+
+    return this;
 };
+
+function Viewport(opts)
+{
+
+    return this;
+}
+
+function BasemapLayer(opts) 
+{
+
+    return this;
+}
+
+BasemapLayer.prototype.install = function (map) {
+
+};
+
+function RasterLayer(opts)
+{
+
+    return this;
+}
+
+RasterLayer.prototype.install = function (map) {
+
+};
+
+RasterLayer.prototype.render = function (viewport) {
+
+};
+
+function VectorLayer(opts)
+{
+
+    return this;
+}
+
+VectorLayer.prototype.install = function (map) {
+
+};
+
+VectorLayer.prototype.render = function (viewport) {
+
+};
+
+function Plugin(opts)
+{
+
+    return this;
+}
